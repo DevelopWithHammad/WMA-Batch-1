777a1c1ce617362d822c800443e8ea1d3e06a372
Hammad <hammadurrehman@gmail.com/>
2023-10-21 23:53:24 +0500

    math_expression_unfamiliar

---
+++ JavaScript/Math_expression_unfamiliar/app.js
@@ -0,0 +1,46 @@
+var sum = 2 + 2;
+
+alert(typeof sum);
+alert(sum);
+
+console.log(2 + 2)
+
+console.log(2 + 2)
+
+
+// ==========  Math Familiar Operator ================
+
+// addition
+var sum = 2 + 2;
+console.log(sum);
+
+// substraction
+var sum = 10 - 2;
+console.log(sum);
+
+// multiplication
+var sum = 10 * 2;
+console.log(sum);
+
+// division
+var sum = 10 / 5;
+console.log(sum);
+
+// modulus
+var sum = 10 % 5;
+console.log(sum);
+
+// power
+var sum = 4 ** 2;
+console.log(sum);
+
+
+
+
+var greetMessage = prompt("What is your name");
+console.log("Welcome "  + greetMessage +  " to our website");
+
+var myName =prompt ("what is your name.?");
+var whoIAm = prompt("what work your do..?");
+
+console.log("My name is " + myName + " and I am " + whoIAm)
